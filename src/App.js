d7468a9ac9b7068e36c038613713b5ec660ad4f4
Adam Faryna <adamfaryna@appdy.net>
2017-10-31 04:18:24 +0000

    Added React entry point.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2017 Adam Faryna <adamfaryna@appdy.net>
+ *
+ * Distributed under terms of the BSD 2-Clause license.
+ */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    /* const {} = this.props; */
+
+    return (
+      <div>
+      </div>
+    );
+  };
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@
  *
  * Distributed under terms of the BSD 2-Clause license.
  */
+import React from 'react';
+import { render } from 'react-dom';
 
+import App from './App';
+
+render(
+  <App />
+, document.getElementById('root'));
