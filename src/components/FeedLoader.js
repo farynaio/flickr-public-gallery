f2fac51847bc5cce1decf279ba798716abae2da0
Adam Faryna <adamfaryna@appdy.net>
2017-11-01 02:52:44 +0000

    Added FeedLoader.

---
+++ src/components/FeedLoader.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Adam Faryna <adamfaryna@appdy.net>
+ *
+ * Distributed under terms of the BSD 2-Clause license.
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class FeedLoader extends Component {
+  // static get defaultProps = {
+  // }
+  //
+  // static propTypes = {
+  //   ...Component.defaultProps,
+  // }
+
+  render() {
+    const {} = this.props;
+
+    return (
+      <div className=''>
+
+      </div>
+    );
+  };
+}
+
+export default FeedLoader;
---
+++ src/containers/ConnectedFeedLoader.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 Adam Faryna <adamfaryna@appdy.net>
+ *
+ * Distributed under terms of the BSD 2-Clause license.
+ */
+import { connect } from 'react-redux';
+
+import FeedLoader from '../components/FeedLoader';
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FeedLoader);
